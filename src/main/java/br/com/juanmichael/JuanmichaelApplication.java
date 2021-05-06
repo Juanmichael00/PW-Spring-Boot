0e6ad6464091a86d82734e476c2e6273dde72fd2
Juanmichael00 <juanmichaeltec@hotmail.com>
2021-05-06 00:33:06 -0300

    Spring-Tools-basic

---
+++ src/main/java/br/com/juanmichael/JuanmichaelApplication.java
@@ -0,0 +1,13 @@
+package br.com.juanmichael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuanmichaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuanmichaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/juanmichael/JuanmichaelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juanmichael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuanmichaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
